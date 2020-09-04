662ad0d157095b85d87e4aaa3cf372ed8bdbfe8c
lynsay <woodiq1000@163.com>
2020-09-04 14:51:39 +0800

    第一次提交项目

---
+++ guliamll-coupon/src/main/java/com/atguigu/gulimall/coupon/GuliamllCouponApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliamllCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuliamllCouponApplication.class, args);
+	}
+
+}
